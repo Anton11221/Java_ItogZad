701b8f6186080dd39575ab2a4473cc4c2d46290b
Anton11221 <xxx-i@ya.ru>
2023-12-19 12:23:41 +0300

    Итоговое задание №2 "Решение уравнений"

--- src/EquationSolver.java
+++ src/EquationSolver.java
@@ -10,7 +10,7 @@ public class EquationSolver {
 
         //Разбиваем уравнение на компаненты
         char s1 = equation.charAt(0);
-        char s2 = equation.charAt(1);
+        char operator = equation.charAt(1);
         char s3 = equation.charAt(2);
         char s5 = equation.charAt(4);
 
@@ -18,24 +18,25 @@ public class EquationSolver {
 
         // Решаем уравнение в зависимости от положения неизвестного x и символа '+' или '-'
         if (s1 == 'x') {
-            if (s2 == '+') {
+            if (operator == '+') {
                 x = Character.getNumericValue(s5) - Character.getNumericValue(s3);
-            } else if (s2 == '-') {
+            } else if (operator == '-') {
                 x = Character.getNumericValue(s5) + Character.getNumericValue(s3);
             }
         } else if (s3 == 'x') {
-            if (s2 == '+') {
+            if (operator == '+') {
                 x = Character.getNumericValue(s5) - Character.getNumericValue(s1);
-            } else if (s2 == '-') {
+            } else if (operator == '-') {
                 x = Character.getNumericValue(s5) + Character.getNumericValue(s1);
             }
         } else if (s5 == 'x') {
-            if (s2 == '+') {
+            if (operator == '+') {
                 x = Character.getNumericValue(s1) + Character.getNumericValue(s3);
-            } else if (s2 == '-') {
+            } else if (operator == '-') {
                 x = Character.getNumericValue(s1) - Character.getNumericValue(s3);
             }
         }
-        System.out.println("X = " +x); // Выводим значение x
+        // Выводим значение x
+        System.out.println("X = " +x);
     }
 }
